da9af302ae2caece81ca63a7858f04bc86e5fc69
Simion Gitau <simiongitau33@gmail.com>
2022-08-22 12:27:30 +0300

    well

--- src/component/about/About.js
+++ src/component/about/About.js
@@ -1,11 +1,168 @@
 import React from "react";
-import styled from "styled-components";
+import maize from "../../assert/maize.jpg";
+import Footer from "../footer/Footer";
 export default function About() {
-  const Container = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding-top: 250px;
-  `;
-  return <Container style={{ color: "red" }}>under construction!</Container>;
+  return (
+    <div className="pt-2 px-2">
+      {/* first div */}
+      <div className="">
+        <img
+          src={maize}
+          alt="about"
+          className="w-full h-[35vh] object-cover opacity-80 rounded"
+        />
+        <div className="flex items-center pt-2">
+          <h2 className="font-bold capitalize text-[45px] mr-4 text-gray-500">
+            eldo agrovet <br /> limited
+          </h2>
+          <p className=" text-justify font-extralight max-w-[75%]">
+            Biovision Foundation is a Swiss non-profit organization with a
+            global mission to alleviate poverty and improve the livelihoods of
+            rural people in Africa while maintaining the natural resources and
+            diversity that sustains life. Our chairman is Dr. Hans Rudolf
+            Herren, one of the world's leading researchers in biological pest
+            control. He has been living and researching in Africa for over 20
+            years. In 1995 he won the World Food Prize, the first Swiss to
+            receive this honour. From 1994 until 2005 he has been the CEO and
+            Director of the International Centre of Insect Physiology and
+            Ecology (icipe) in Nairobi, Kenya. In May 2005 Dr. Herren officially
+            took over presidency of the internationally active Millenium
+            Institute in Washington DC. This institute supports the governments
+            of developing countries by providing resources for sustainable
+            development. While icipe plays a major role in the control of pests,
+            parasites and disease vectors in developing countries. Biovision
+            Foundation functions as an intermediary between research institutes
+            and local users in order to ensure that benefits of science reach
+            the people who need them most.
+          </p>
+        </div>
+      </div>
+      {/* image */}
+      {/* compny info */}
+      {/* end of first div */}
+      <div className="font-bold capitalize text-center my-2 text-xl">
+        education
+      </div>
+      <div className="pt-2 grid grid-cols-4 gap-4 pb-2">
+        <div className=" p-4 rounded flex justify-center flex-col items-center shadow-md shadow-gray-400 ">
+          <h3 className="capitalize font-bold my-2">feed nutrition</h3>
+          <p className="font-extralight text-justify">
+            Animal feed needs to meet the requirements of the animal. For cattle
+            it must contain a certain structure to keep the stomach healthy, and
+            for all animals it must contain dry matter, various groups of
+            nutrients, minerals and trace-elements and should not be mouldy or
+            mixed with dirt and soil nor contain poisonous ingredients (for
+            example pesticides or herbicides on crop residues). The composition
+            of feeds in a ration depends on the type of animals being fed and
+            their stage of production. Cattle and goats, being ruminants, are
+            better equipped to digest crude fibre than pigs or chickens. Young
+            animals need more protein than adult animals, while animals in milk
+            need a higher protein content in the ration than non lactating
+            animals. Generally animals must have a ration containing: Energy
+            (from carbohydrates and fats) to maintain the body and produce
+            (milk, meat, work). The carbohydrates and fats not needed for
+            production are converted to fat and stored in the body. Protein is
+            needed for body building (growth) and maintenance as well as milk
+            production. Without protein there would be no body weight gain nor
+            milk production. Excess protein is converted to urea and fat
+            Minerals help in body building as well as in biological regulation
+            of growth and reproduction. They are also a major source of
+            nutrients in milk. Vitamins help regulate the biological processes
+            in the body and become a source of nutrients in milk Water helps all
+            over in body building, heat regulation, biological processes as well
+            as a large constituent of milk production as well as eggs.
+          </p>
+        </div>
+        <div className=" p-4 rounded flex justify-center flex-col items-center shadow-md shadow-gray-400 ">
+          <h3 className="capitalize font-bold my-2">feed nutrition</h3>
+          <p className="font-extralight text-justify">
+            Animal feed needs to meet the requirements of the animal. For cattle
+            it must contain a certain structure to keep the stomach healthy, and
+            for all animals it must contain dry matter, various groups of
+            nutrients, minerals and trace-elements and should not be mouldy or
+            mixed with dirt and soil nor contain poisonous ingredients (for
+            example pesticides or herbicides on crop residues). The composition
+            of feeds in a ration depends on the type of animals being fed and
+            their stage of production. Cattle and goats, being ruminants, are
+            better equipped to digest crude fibre than pigs or chickens. Young
+            animals need more protein than adult animals, while animals in milk
+            need a higher protein content in the ration than non lactating
+            animals. Generally animals must have a ration containing: Energy
+            (from carbohydrates and fats) to maintain the body and produce
+            (milk, meat, work). The carbohydrates and fats not needed for
+            production are converted to fat and stored in the body. Protein is
+            needed for body building (growth) and maintenance as well as milk
+            production. Without protein there would be no body weight gain nor
+            milk production. Excess protein is converted to urea and fat
+            Minerals help in body building as well as in biological regulation
+            of growth and reproduction. They are also a major source of
+            nutrients in milk. Vitamins help regulate the biological processes
+            in the body and become a source of nutrients in milk Water helps all
+            over in body building, heat regulation, biological processes as well
+            as a large constituent of milk production as well as eggs.
+          </p>
+        </div>
+        <div className=" p-4 rounded flex justify-center flex-col items-center shadow-md shadow-gray-400 ">
+          <h3 className="capitalize font-bold my-2">feed nutrition</h3>
+          <p className="font-extralight text-justify">
+            Animal feed needs to meet the requirements of the animal. For cattle
+            it must contain a certain structure to keep the stomach healthy, and
+            for all animals it must contain dry matter, various groups of
+            nutrients, minerals and trace-elements and should not be mouldy or
+            mixed with dirt and soil nor contain poisonous ingredients (for
+            example pesticides or herbicides on crop residues). The composition
+            of feeds in a ration depends on the type of animals being fed and
+            their stage of production. Cattle and goats, being ruminants, are
+            better equipped to digest crude fibre than pigs or chickens. Young
+            animals need more protein than adult animals, while animals in milk
+            need a higher protein content in the ration than non lactating
+            animals. Generally animals must have a ration containing: Energy
+            (from carbohydrates and fats) to maintain the body and produce
+            (milk, meat, work). The carbohydrates and fats not needed for
+            production are converted to fat and stored in the body. Protein is
+            needed for body building (growth) and maintenance as well as milk
+            production. Without protein there would be no body weight gain nor
+            milk production. Excess protein is converted to urea and fat
+            Minerals help in body building as well as in biological regulation
+            of growth and reproduction. They are also a major source of
+            nutrients in milk. Vitamins help regulate the biological processes
+            in the body and become a source of nutrients in milk Water helps all
+            over in body building, heat regulation, biological processes as well
+            as a large constituent of milk production as well as eggs.
+          </p>
+        </div>
+        <div className=" p-4 rounded flex justify-center flex-col items-center shadow-md shadow-gray-400 ">
+          <h3 className="capitalize font-bold my-2">feed nutrition</h3>
+          <p className="font-extralight text-justify">
+            Animal feed needs to meet the requirements of the animal. For cattle
+            it must contain a certain structure to keep the stomach healthy, and
+            for all animals it must contain dry matter, various groups of
+            nutrients, minerals and trace-elements and should not be mouldy or
+            mixed with dirt and soil nor contain poisonous ingredients (for
+            example pesticides or herbicides on crop residues). The composition
+            of feeds in a ration depends on the type of animals being fed and
+            their stage of production. Cattle and goats, being ruminants, are
+            better equipped to digest crude fibre than pigs or chickens. Young
+            animals need more protein than adult animals, while animals in milk
+            need a higher protein content in the ration than non lactating
+            animals. Generally animals must have a ration containing: Energy
+            (from carbohydrates and fats) to maintain the body and produce
+            (milk, meat, work). The carbohydrates and fats not needed for
+            production are converted to fat and stored in the body. Protein is
+            needed for body building (growth) and maintenance as well as milk
+            production. Without protein there would be no body weight gain nor
+            milk production. Excess protein is converted to urea and fat
+            Minerals help in body building as well as in biological regulation
+            of growth and reproduction. They are also a major source of
+            nutrients in milk. Vitamins help regulate the biological processes
+            in the body and become a source of nutrients in milk Water helps all
+            over in body building, heat regulation, biological processes as well
+            as a large constituent of milk production as well as eggs.
+          </p>
+        </div>
+      </div>
+      {/* div of eductaion */}
+      <Footer />
+    </div>
+  );
 }
